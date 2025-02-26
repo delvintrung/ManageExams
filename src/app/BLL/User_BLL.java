4c5772e7bdf7a9606596cd9d9496ae083067365f
thevu29 <vnguyen132az@gmail.com>
2025-02-26 15:02:23 +0700

    hash password, fix login

--- src/app/BLL/User_BLL.java
+++ src/app/BLL/User_BLL.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import app.DAL.User_DAL;
 import app.DTO.User_DTO;
 import app.database.ConnectDatabase;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class User_BLL {
 	public User_DAL userDAL = new User_DAL();
@@ -17,6 +18,10 @@ public class User_BLL {
 	public static User_DTO getInstance() {
         return new User_DTO();
     }
+	
+	private String hashPassword(String password) {
+		return BCrypt.hashpw(password, BCrypt.gensalt(12));
+	}
     
     public ArrayList<User_DTO> selectAll() {
         ArrayList<User_DTO> result = new ArrayList<User_DTO>();
@@ -44,6 +49,11 @@ public class User_BLL {
     }
     
     public boolean create(User_DTO user) {    	
+    	if (!user.getUserPassword().equals("")) {
+    		String hashedPassword = hashPassword(user.getUserPassword());
+    		user.setUserPassword(hashedPassword);
+    	}
+    	
         if (userDAL.create(user) != 0) {
         	users.add(user);
             return true;
